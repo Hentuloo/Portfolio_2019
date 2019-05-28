bf811172af207c1cce82f7ba0ce0f1ed7cee6b6e
Kamil <Kamil@>
2019-05-28 16:24:52 +0200

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+// import { Link } from 'gatsby';
+
+const index = () => <div>Stronka moja</div>;
+
+export default index;
---
+++ src/pages/page-2.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SecondPage = () => <div>Second</div>;
+
+export default SecondPage;
