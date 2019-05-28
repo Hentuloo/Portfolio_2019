ac0f03b0a8321a5e76f456d191eec433dbfd0f55
Kamil <Kamil@>
2019-05-28 17:11:45 +0200

    add GlobalStyle and update mainTheme

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 // import { Link } from 'gatsby';
 
+import GlobalStyle from 'themes/GlobalStyles';
 import { theme } from 'themes/mainTheme';
 
 const Wrapper = styled.div`
-  background-color: ${() => theme.bluePrimary};
+  background-color: ${() => theme.redSecondary};
 `;
 const index = () => (
   <ThemeProvider theme={theme}>
-    <Wrapper>Stronka moja</Wrapper>
+    <>
+      <GlobalStyle />
+      <Wrapper>Stronka moja</Wrapper>
+    </>
   </ThemeProvider>
 );
 
