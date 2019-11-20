8e03b2c2bdea334095e45da8752ef9253325bdc2
Kamil <Kamil@>
2019-11-20 18:53:14 +0100

    increased waiting time for projectsBoxes

--- src/templates/ProjectsTemplate.js
+++ src/templates/ProjectsTemplate.js
@@ -15,105 +15,105 @@ opacity:1;
 `;
 
 const ProjectsBox = styled.section`
-  margin: 30px 0px;
-  z-index: 3;
+    margin: 30px 0px;
+    z-index: 3;
 
-  @media (min-width: ${({ theme }) => theme.breakPointMobile}) {
-    flex-basis: 60%;
-    min-width: 400px;
-    min-height: 200px;
-    margin: 40px 0px;
-    & > div {
-      min-height: 210px;
+    @media (min-width: ${({ theme }) => theme.breakPointMobile}) {
+        flex-basis: 60%;
+        min-width: 400px;
+        min-height: 200px;
+        margin: 40px 0px;
+        & > div {
+            min-height: 210px;
+        }
     }
-  }
-  @media (min-width: ${({ theme }) => theme.breakPointMiddle}) {
-    flex-basis: 45%;
-  }
+    @media (min-width: ${({ theme }) => theme.breakPointMiddle}) {
+        flex-basis: 45%;
+    }
 `;
 const MarkdownWrapper = styled.div`
-  width: 90%;
-  margin: 0px auto;
-  opacity: 0;
-  animation: ${opacity} 0.5s linear forwards;
-  position: relative;
-  @media (min-width: ${({ theme }) => theme.breakPointMobile}) {
-    width: 80%;
-    max-width: 1100px;
-    margin-bottom: 30px;
-    margin-top: 20px;
-    &::before {
-      content: '';
-      width: 120px;
-      height: 80px;
-      position: absolute;
-      bottom: 0%;
-      right: 0%;
-      background-color: ${({ theme }) => theme.redFirst};
-      transform: translate(45%, 45%);
+    width: 90%;
+    margin: 0px auto;
+    opacity: 0;
+    animation: ${opacity} 0.5s linear forwards;
+    position: relative;
+    @media (min-width: ${({ theme }) => theme.breakPointMobile}) {
+        width: 80%;
+        max-width: 1100px;
+        margin-bottom: 30px;
+        margin-top: 20px;
+        &::before {
+            content: '';
+            width: 120px;
+            height: 80px;
+            position: absolute;
+            bottom: 0%;
+            right: 0%;
+            background-color: ${({ theme }) => theme.redFirst};
+            transform: translate(45%, 45%);
+        }
     }
-  }
 `;
 const ProjectsWrapper = styled.div`
-  display: block;
-  margin-bottom: 150px;
-  width: 100%;
-  overflow: hidden;
-  padding: 0px 20px;
-  @media (min-width: ${({ theme }) => theme.breakPointMobile}) {
-    width: 95%;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    margin: 0px auto;
-  }
+    display: block;
+    margin-bottom: 150px;
+    width: 100%;
+    overflow: hidden;
+    padding: 0px 20px;
+    @media (min-width: ${({ theme }) => theme.breakPointMobile}) {
+        width: 95%;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        margin: 0px auto;
+    }
 `;
 const Wrapper = styled.div`
-  width: 100%;
-  min-height: 110vh;
-  margin: 30px auto 0px auto;
+    width: 100%;
+    min-height: 110vh;
+    margin: 30px auto 0px auto;
 `;
 class Projects extends Component {
-  state = {
-    markdownLoaded: false,
-  };
+    state = {
+        markdownLoaded: false,
+    };
 
-  componentDidMount() {
-    const { markdownLoaded } = this.state;
-    if (!markdownLoaded) {
-      setTimeout(() => {
-        this.setState({ markdownLoaded: true });
-      }, 500);
+    componentDidMount() {
+        const { markdownLoaded } = this.state;
+        if (!markdownLoaded) {
+            setTimeout(() => {
+                this.setState({ markdownLoaded: true });
+            }, 900);
+        }
+        return null;
     }
-    return null;
-  }
 
-  render() {
-    const { markdownLoaded } = this.state;
-    const { projects, markdownContent } = this.props;
-    return (
-      <Wrapper>
-        <MarkdownWrapper>
-          <Markdown markdown={markdownContent} />
-        </MarkdownWrapper>
-        {markdownLoaded && (
-          <ProjectsWrapper>
-            {projects.map(e => (
-              <ProjectsBox key={e.id}>
-                <ProjectBox data={e} />
-              </ProjectsBox>
-            ))}
-          </ProjectsWrapper>
-        )}
-      </Wrapper>
-    );
-  }
+    render() {
+        const { markdownLoaded } = this.state;
+        const { projects, markdownContent } = this.props;
+        return (
+            <Wrapper>
+                <MarkdownWrapper>
+                    <Markdown markdown={markdownContent} />
+                </MarkdownWrapper>
+                {markdownLoaded && (
+                    <ProjectsWrapper>
+                        {projects.map(e => (
+                            <ProjectsBox key={e.id}>
+                                <ProjectBox data={e} />
+                            </ProjectsBox>
+                        ))}
+                    </ProjectsWrapper>
+                )}
+            </Wrapper>
+        );
+    }
 }
 
 Projects.propTypes = {
-  markdownContent: PropTypes.string.isRequired,
-  projects: PropTypes.arrayOf(Object).isRequired,
+    markdownContent: PropTypes.string.isRequired,
+    projects: PropTypes.arrayOf(Object).isRequired,
 };
 
 export default Projects;
