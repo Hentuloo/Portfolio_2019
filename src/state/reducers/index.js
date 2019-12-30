5999e7910ec9949b9fb38dd831ebf8e6a97e16e7
Kamil <Kamil@>
2019-12-30 16:28:06 +0100

    change capital file name in git repositiory (cursor.js)

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,7 +1,17 @@
 import { combineReducers } from 'redux';
+import { cursorActions } from 'state/actions/cursorActions';
 import language from './language';
 import ActivePage from './ActivePage';
-import cursor from './cursor';
+// import cursor from './cursor';
+
+const cursor = (state = 'normal', action) => {
+    switch (action.type) {
+        case cursorActions.CHANGE_MOOD:
+            return action.payload;
+        default:
+            return state;
+    }
+};
 
 export default combineReducers({
     language,
